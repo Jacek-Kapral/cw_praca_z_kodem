85cf43fcf416a6620e28bd3b52ab200d64e1e664
vdi-terminal <vdi-terminal@EDU-WSB.local>
2022-04-03 14:19:19 +0200

    Lab03 postep 1

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+if __name__ == "__main__":
+    app.run(debug=True)
